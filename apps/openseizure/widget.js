d86cfef98093a4adcb06608b26cf1b21317c12ab
Graham Jones <grahamjones139@gmail.com>
2023-09-27 22:09:32 +0100

    Updated UUIDs to align with current (V4.2.x) version of the OSD android app.

--- apps/openseizure/widget.js
+++ apps/openseizure/widget.js
@@ -24,11 +24,11 @@
 	accelIdx = 0;
 	batteryLevel = E.getBattery();
       try { NRF.updateServices({
-        "a19585e9-0001-39d0-015f-b3e2b9a0c854" : {
-          "a19585e9-0002-39d0-015f-b3e2b9a0c854" : {
+        "000085e9-0000-1000-8000-00805f9b34fb" : {
+          "000085ea-0000-1000-8000-00805f9b34fb" : {
             value : accelData, notify : true
           },
-          "a19585e9-0004-39d0-015f-b3e2b9a0c854" : {
+          "000085eb-0000-1000-8000-00805f9b34fb" : {
               value : batteryLevel, notify : true
           }
         }
@@ -37,14 +37,14 @@
   });
 
     NRF.setServices({
-	"a19585e9-0001-39d0-015f-b3e2b9a0c854" : {
-    "a19585e9-0002-39d0-015f-b3e2b9a0c854" : {
+	"000085e9-0000-1000-8000-00805f9b34fb" : {
+    "000085ea-0000-1000-8000-00805f9b34fb" : {
 		value : accelData,
 		maxLen : 20,
 		readable : true,
 		notify : true
     },
-    "a19585e9-0004-39d0-015f-b3e2b9a0c854" : {
+    "000085eb-0000-1000-8000-00805f9b34fb" : {
 		value : batteryLevel,
 		maxLen : 20,
 		readable : true,
