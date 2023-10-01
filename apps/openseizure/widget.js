50f09521d9375a172da83bbd89202fb22c69f7c7
Graham Jones <grahamjones139@gmail.com>
2023-10-01 20:21:07 +0100

    V0.03 - changed characteristic IDs and made them constants at the top of the .js file

--- apps/openseizure/widget.js
+++ apps/openseizure/widget.js
@@ -4,57 +4,61 @@
  *
  * 07 August 2020 - Gordon Williams:  Initial version providing an accelerometer data service.
  * 12 August 2020 - Graham Jones:  Added a battery percentage service.
-*/
+ */
+
+const SERV_OSD =          "000085e9-0000-1000-8000-00805f9b34fb";
+const CHAR_OSD_ACC_DATA = "000085ea-0000-1000-8000-00805f9b34fb";
+const CHAR_OSD_BAT_DATA = "000085eb-0000-1000-8000-00805f9b34fb";
 
 (() => {
-  function draw() {
-    g.reset();
-    g.drawImage(E.toArrayBuffer(atob("GBiEBAAAAAABEREQAAAAAAAAAAAKmZkgAAAAAAAAAAAKmZkgAAAAAAAAAAAKkzkgAAAAAAACIQAKkzkgABIgAAAZmSAKPykgApmRAAApmZoqMjkiqZmSAAKZmZmZ8zmpmZmZIAKZmZmZMzmZmZmZIAEpmZmZkzqZmZmSEAACqZmZkjOZmZogAAAAApmZkjOZmSAAAAAAApmZn/mZmSAAAAACqZmZrymZmZogAAEpmZmZkzmZmZmSEAqZmZmZkjqZmZmZoAKZmZmamvmpmZmZIAApmZIan6mhKZmSAAAZmiAKkymgAqmRAAACIAAKkimgAAIgAAAAAAAKkimgAAAAAAAAAAAKmZmgAAAAAAAAAAAKmZmgAAAAAAAAAAABEREQAAAAAA==")), this.x, this.y);
-  }
+    function draw() {
+	g.reset();
+	g.drawImage(E.toArrayBuffer(atob("GBiEBAAAAAABEREQAAAAAAAAAAAKmZkgAAAAAAAAAAAKmZkgAAAAAAAAAAAKkzkgAAAAAAACIQAKkzkgABIgAAAZmSAKPykgApmRAAApmZoqMjkiqZmSAAKZmZmZ8zmpmZmZIAKZmZmZMzmZmZmZIAEpmZmZkzqZmZmSEAACqZmZkjOZmZogAAAAApmZkjOZmSAAAAAAApmZn/mZmSAAAAACqZmZrymZmZogAAEpmZmZkzmZmZmSEAqZmZmZkjqZmZmZoAKZmZmamvmpmZmZIAApmZIan6mhKZmSAAAZmiAKkymgAqmRAAACIAAKkimgAAIgAAAAAAAKkimgAAAAAAAAAAAKmZmgAAAAAAAAAAAKmZmgAAAAAAAAAAABEREQAAAAAA==")), this.x, this.y);
+    }
 
-  var accelData = new Uint8Array(20);
+    var accelData = new Uint8Array(20);
     var accelIdx = 0;
     var batteryLevel = 0;
-  //http://kionixfs.kionix.com/en/datasheet/KX023-1025%20Specifications%20Rev%2012.0.pdf
-  Bangle.accelWr(0x1B,0x01 | 0x40); // 25hz output, ODR/2 filter
-  Bangle.setPollInterval(40); // 25hz input
-  Bangle.on('accel',function(a) {
-    accelData[accelIdx++] = E.clip(a.mag*64,0,255);
-    if (accelIdx>=accelData.length) {
-	accelIdx = 0;
-	batteryLevel = E.getBattery();
-      try { NRF.updateServices({
-        "000085e9-0000-1000-8000-00805f9b34fb" : {
-          "000085ea-0000-1000-8000-00805f9b34fb" : {
-            value : accelData, notify : true
-          },
-          "000085eb-0000-1000-8000-00805f9b34fb" : {
-              value : batteryLevel, notify : true
-          }
-        }
-      });} catch(e) {}
-    }
-  });
-
+    //http://kionixfs.kionix.com/en/datasheet/KX023-1025%20Specifications%20Rev%2012.0.pdf
+    Bangle.accelWr(0x1B,0x01 | 0x40); // 25hz output, ODR/2 filter
+    Bangle.setPollInterval(40); // 25hz input
+    Bangle.on('accel',function(a) {
+	accelData[accelIdx++] = E.clip(a.mag*64,0,255);
+	if (accelIdx>=accelData.length) {
+	    accelIdx = 0;
+	    batteryLevel = E.getBattery();
+	    try { NRF.updateServices({
+		SERV_OSD : {
+		    CHAR_OSD_ACC_DATA : {
+			value : accelData, notify : true
+		    },
+		    CHAR_OSD_BAT_DATA : {
+			value : batteryLevel, notify : true
+		    }
+		}
+	    });} catch(e) {}
+	}
+    });
+    
     NRF.setServices({
-	"000085e9-0000-1000-8000-00805f9b34fb" : {
-    "000085ea-0000-1000-8000-00805f9b34fb" : {
+	SERV_OSD : {
+	    CHAR_OSD_ACC_DATA : {
 		value : accelData,
 		maxLen : 20,
 		readable : true,
 		notify : true
-    },
-    "000085eb-0000-1000-8000-00805f9b34fb" : {
+	    },
+	    CHAR_OSD_BATT_DATA: {
 		value : batteryLevel,
 		maxLen : 20,
 		readable : true,
 		notify : true
-    }
+	    }
 	}
     });
-
-  // add your widget
-  WIDGETS["openseizure"]={
-    area:"tl", width: 24, draw:draw
-  };
+    
+    // add your widget
+    WIDGETS["openseizure"]={
+	area:"tl", width: 24, draw:draw
+    };
 })();
