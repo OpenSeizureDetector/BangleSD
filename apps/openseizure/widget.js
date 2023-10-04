a926d276b8bb8d213d7eef88d823f865fe99c129
Graham Jones <grahamjones139@gmail.com>
2023-10-04 21:40:17 +0100

    Revert "V0.07 - Removed OSD HR char (using official one instead)"
    This reverts commit e2373e34a995d90870cfb21e1074864926d22534.

--- apps/openseizure/widget.js
+++ apps/openseizure/widget.js
@@ -46,14 +46,20 @@ const CHAR_HR_LOC = 0x2A38; // Official BLE Sensor Location UUID
 				value : batteryLevel,
 				notify : true
 			};
+			var charOsdHrData = {
+				value : hrVal,
+				notify : true
+			};
 			var charBleHrm = {
 				value : [0x06, hrVal],   // Check what 0x06 is?
 				notify : true
 			};
 		
+		
 			var servOsd = {};
 			servOsd[CHAR_OSD_ACC_DATA] = charOsdAccData;
 			servOsd[CHAR_OSD_BAT_DATA] = charOsdBatData;
+			servOsd[CHAR_OSD_HR_DATA] = charOsdHrData;
 			var servHrm = {};
 			servHrm[CHAR_HRM] = charBleHrm;
 		
@@ -93,6 +99,12 @@ const CHAR_HR_LOC = 0x2A38; // Official BLE Sensor Location UUID
 		readable : true,
 		notify : true
 	};
+	var charOsdHrData = {
+		value : hrVal,
+		maxLen : 8,
+		readable : true,
+		notify : true
+	};
 	var charBleHrm = {
 		value : [0x06, 0],   // Check what 0x06 is?
 		maxLen : 2,
@@ -105,6 +117,7 @@ const CHAR_HR_LOC = 0x2A38; // Official BLE Sensor Location UUID
 	var servOsd = {};
 	servOsd[CHAR_OSD_ACC_DATA] = charOsdAccData;
 	servOsd[CHAR_OSD_BAT_DATA] = charOsdBatData;
+	servOsd[CHAR_OSD_HR_DATA] = charOsdHrData;
 	var servHrm = {};
 	servHrm[CHAR_HRM] = charBleHrm;
 	servHrm[CHAR_HR_LOC] = charBleHrLoc;
